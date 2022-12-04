de4b0b82bd3eecbc95257ee1ddde57aef8268c42
Naura Sharfina Azarine <81897660+knimoni@users.noreply.github.com>
2022-12-05 00:41:25 +0700

    erd und sql

---
+++ tpn_db.sql
@@ -0,0 +1,301 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2022 at 06:40 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tpn_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `children`
+--
+
+CREATE TABLE `children` (
+  `children_id` int(2) NOT NULL,
+  `children_name` varchar(84) NOT NULL,
+  `children_identifier` varchar(12) NOT NULL,
+  `children_bloodtype` varchar(3) NOT NULL,
+  `children_birthday` varchar(50) NOT NULL,
+  `supervisor_id` int(1) NOT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `children`
+--
+
+INSERT INTO `children` (`children_id`, `children_name`, `children_identifier`, `children_bloodtype`, `children_birthday`, `supervisor_id`, `is_deleted`) VALUES
+(3, 'Ray', '81194', 'AB', 'January 16, 2034', 1, 0),
+(4, 'Phil', '34394', 'O', 'July 31, 2041', 1, 0),
+(6, 'Lucas', 'KGX2A7', 'B', 'November 28, 2017', 2, 0),
+(22, 'Oliver', 'XII123244', 'AB', 'July 22', 3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farm`
+--
+
+CREATE TABLE `farm` (
+  `farm_id` int(2) NOT NULL,
+  `farm_name` varchar(100) NOT NULL,
+  `farm_identifier` varchar(12) NOT NULL,
+  `supervisor_id` int(1) DEFAULT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `farm`
+--
+
+INSERT INTO `farm` (`farm_id`, `farm_name`, `farm_identifier`, `supervisor_id`, `is_deleted`) VALUES
+(3, 'Grace Field House', '194', 1, 0),
+(4, 'Grace Field House', '194', 1, 0),
+(5, 'Glory Bell', '333', 2, 0),
+(6, 'Glory Bell', '333', 2, 0),
+(7, 'Grand Valley', '139', 3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supervisor`
+--
+
+CREATE TABLE `supervisor` (
+  `supervisor_id` int(1) NOT NULL,
+  `supervisor_name` varchar(84) NOT NULL,
+  `supervisor_identifier` varchar(7) NOT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `supervisor`
+--
+
+INSERT INTO `supervisor` (`supervisor_id`, `supervisor_name`, `supervisor_identifier`, `is_deleted`) VALUES
+(1, 'Isabella', '73584', 0),
+(2, 'Krone', '18684', 0),
+(3, 'Matilda', '60684', 0),
+(4, 'Minerva', '22714', 0),
+(5, 'Grandma', '88444', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'nauracoba', 'cobaterus@gmail.com', NULL, '$2y$10$fsKh1xnUisXvzZPs1RSoKu11ZvfzZrSLsP/7bf4kFuSYmculZEuh.', 'mMO6LSGZpAAPcqHJC8AYrVGEjSWZEO4MOjGi3zSeQm1BNZxymhfzu9LKky6H', '2022-12-04 06:18:46', '2022-12-04 06:18:46'),
+(2, 'cobanaura2', 'cobalagi@naura.com', NULL, '$2y$10$zem/t7S/m3zIIUd.dOJvVOf0gDaqZtTXTAz3yyZTTfO74jQ1JSBxu', 'a7zfoXuNDWUo3wrec6ioDQUXilfdZPOriTmW5doDdP1MlDYgJbXyuwWjUZoU', '2022-12-04 06:30:22', '2022-12-04 06:30:22'),
+(3, 'naura', 'naura@gmail.com', NULL, '$2y$10$pvFxa4erkClTuDS/3G7K2eTwEm1QaveaVBhoPR1NegcYR39t9YNe.', NULL, '2022-12-04 07:39:35', '2022-12-04 07:39:35'),
+(4, 'naura', 'naura@azarine.com', NULL, '$2y$10$wZ9ysd7fg5kSQs9P2wLYMeIn3Pi.F15G2nnu/PteqqDsZFhqE/.oG', NULL, '2022-12-04 07:58:13', '2022-12-04 07:58:13'),
+(5, 'Naura Sharfina Azarine', 'gantilagi@gmail.com', NULL, '$2y$10$3Ztu8y4ODQl30cQFDXoXoeHQ5FUEXXRLzlKoPgD6oEpHnpFcOAxTO', NULL, '2022-12-04 09:51:35', '2022-12-04 09:51:35');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `children`
+--
+ALTER TABLE `children`
+  ADD PRIMARY KEY (`children_id`),
+  ADD KEY `supervisor_id` (`supervisor_id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `farm`
+--
+ALTER TABLE `farm`
+  ADD PRIMARY KEY (`farm_id`),
+  ADD KEY `supervisor_id` (`supervisor_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  ADD PRIMARY KEY (`supervisor_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `children`
+--
+ALTER TABLE `children`
+  ADD CONSTRAINT `children_ibfk_1` FOREIGN KEY (`supervisor_id`) REFERENCES `supervisor` (`supervisor_id`);
+
+--
+-- Constraints for table `farm`
+--
+ALTER TABLE `farm`
+  ADD CONSTRAINT `farm_ibfk_1` FOREIGN KEY (`supervisor_id`) REFERENCES `supervisor` (`supervisor_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
